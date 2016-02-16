24c43c6a877fb68c60f978d7a32b5f115d20d442
Bradford Smith <bsmith8@stevens.edu>
2016-02-16 11:27:56 -0500

    Lab1: Added Labs to repository
    Added skeleton code for Lab1: Inter-Process Communication. Moved Assignments to
    a sub-directory.

---
+++ Labs/Lab1/dp_one.c
@@ -0,0 +1,58 @@
+/* Bradford Smith (bsmith8)
+ * CS 492 Lab 1 dp_one.c
+ * 02/16/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#include <sys/time.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <errno.h>
+#include <unistd.h> /* for sleep */
+
+#define NUMP 5
+
+pthread_mutex_t fork_mutex[NUMP];
+
+int main()
+{
+    int i;
+    pthread_t diner_thread[NUMP];
+    int dn[NUMP];
+    void *diner();
+    for (i = 0; i < NUMP; i++)
+        pthread_mutex_init(&fork_mutex[i], NULL);
+
+    for (i = 0; i < NUMP; i++)
+    {
+        dn[i] = i;
+        pthread_create(&diner_thread[i], NULL, diner, &dn[i]);
+    }
+    for (i = 0; i < NUMP; i++)
+        pthread_join(diner_thread[i], NULL);
+    pthread_exit(0);
+}
+
+void *diner(int *i)
+{
+    int v;
+    int eating = 0;
+    printf("I'm diner %d\n", *i);
+    v = *i;
+    while (eating < 5)
+    {
+        printf("%d is thinking\n", v);
+        sleep(v/2);
+        printf("%d is hungry\n", v);
+        pthread_mutex_lock(&fork_mutex[v]);
+        pthread_mutex_lock(&fork_mutex[(v + 1) % NUMP]);
+        printf("%d is eating\n", v);
+        eating++;
+        sleep(1);
+        printf("%d is done eating\n", v);
+        pthread_mutex_unlock(&fork_mutex[v]);
+        pthread_mutex_unlock(&fork_mutex[(v + 1) % NUMP]);
+    }
+    pthread_exit(NULL);
+}
+
