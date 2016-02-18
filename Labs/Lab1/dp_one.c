9a5d8e5e326c8543dc68eb18e63f5816119c881b
Bradford Smith <bsmith8@stevens.edu>
2016-02-17 20:22:11 -0500

    Lab1: task 2 and lab complete
    Added a pthread condition only allowing 3 philosophers at a time to attempt
    picking up forks and eating.

--- Labs/Lab1/dp_one.c
+++ Labs/Lab1/dp_one.c
@@ -13,6 +13,9 @@
 /* number of philosophers */
 #define NUMP 5
 
+/* number allowed to eat at once */
+#define EAT 3
+
 /* list of mutexes (forks) */
 pthread_mutex_t fork_mutex[NUMP];
 
@@ -20,16 +23,27 @@ pthread_mutex_t fork_mutex[NUMP];
 pthread_mutex_t grab_mutex;
 pthread_mutex_t drop_mutex;
 
+/* pthread condition for eating */
+pthread_cond_t eat_cond;
+
+/* number of philosophers currently eating */
+int num_eating;
+
+
 int main()
 {
     int i;
     pthread_t diner_thread[NUMP];
     int dn[NUMP];
     void *diner();
+    num_eating = 0;
+
+
     for (i = 0; i < NUMP; i++)
         pthread_mutex_init(&fork_mutex[i], NULL);
     pthread_mutex_init(&grab_mutex, NULL);
     pthread_mutex_init(&drop_mutex, NULL);
+    pthread_cond_init(&eat_cond, NULL);
 
     for (i = 0; i < NUMP; i++)
     {
@@ -53,17 +67,25 @@ void *diner(int *i)
         printf("%d is thinking\n", v);
         sleep(v / 2);
         printf("%d is hungry\n", v);
+
         pthread_mutex_lock(&grab_mutex); /* take the grab fork lock */
+        while (num_eating >= EAT)
+        {
+            pthread_cond_wait(&eat_cond, &grab_mutex); /* wait until allowed to eat */
+        }
         pthread_mutex_lock(&fork_mutex[v]); /* take left fork */
         pthread_mutex_lock(&fork_mutex[(v + 1) % NUMP]); /* take right fork */
         pthread_mutex_unlock(&grab_mutex); /* drop the grab fork lock */
         printf("%d is eating\n", v);
+        num_eating++;
         eating++;
         sleep(1);
         printf("%d is done eating\n", v);
         pthread_mutex_lock(&drop_mutex); /* take the drop fork lock */
-        pthread_mutex_unlock(&fork_mutex[v]); /* put left fork */
-        pthread_mutex_unlock(&fork_mutex[(v + 1) % NUMP]); /* put right fork */
+        num_eating--;
+        pthread_mutex_unlock(&fork_mutex[v]); /* drop left fork */
+        pthread_mutex_unlock(&fork_mutex[(v + 1) % NUMP]); /* drop right fork */
+        pthread_cond_broadcast(&eat_cond); /* signal all allowed to eat */
         pthread_mutex_unlock(&drop_mutex); /* drop the drop fork lock */
     }
     pthread_exit(NULL);
