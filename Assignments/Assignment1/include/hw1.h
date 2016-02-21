6da3ea69b8c6795db2d1e6de1d41b95c4c796724
Bradford Smith <bsmith8@stevens.edu>
2016-02-20 22:06:10 -0500

    Assignment1: starting point
    Added header, main and Makefile. Added in required header and linked library for
    pthreads.

---
+++ Assignments/Assignment1/include/hw1.h
@@ -0,0 +1,18 @@
+/* Bradford Smith (bsmith8)
+ * CS 492 Assignment 1 hw1.h
+ * 02/20/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#ifndef _HW1_H_
+#define _HW1_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+
+int main(int argc, char** argv);
+
+#endif /* _HW1_H_ */
---
+++ Assignments/Assignment1/src/main.c
@@ -0,0 +1,13 @@
+/* Bradford Smith (bsmith8)
+ * CS 492 Assignment 1 main.c
+ * 02/20/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#include "hw1.h"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
+
