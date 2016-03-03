f413b118ae74bad313eb045710f7b35771e9799f
Bradford Smith <bsmith8@stevens.edu>
2016-03-03 13:04:41 -0500

    Assignment1: starting to add queue, added report
    Report just contains a placeholder from the assignment about what needs to be
    done.
    
    Created a source file to start building the queue in and changed some function
    types in the header.

--- Assignments/Assignment1/include/q.h
+++ Assignments/Assignment1/include/q.h
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 492 Assignment 1 q.h
- * 02/22/2016
+ * 03/03/2016
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -11,11 +11,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 
 #include "product.h"
 
-void init_q();
-void push_q(void*);
-void* pop_q();
+void init_q(int);
+void push_q(struct product);
+struct product* pop_q();
 
 #endif /* _Q_H_ */
---
+++ Assignments/Assignment1/src/q.c
@@ -0,0 +1,11 @@
+/* Bradford Smith (bsmith9)
+ * CS 492 Assignment 1 q.c
+ * 03/03/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#include "q.h"
+
+void init_q(int size)
+{
+}
