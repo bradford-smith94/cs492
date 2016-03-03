3b7b47c0b6dc8de46fb588b64bb8d56be1948112
Bradford Smith <bsmith8@stevens.edu>
2016-03-03 13:22:50 -0500

    Assignment1: queue initialization
    The queue initialization is finished, the queue will have a static struct of
    products and pointers to the next products.

--- Assignments/Assignment1/src/q.c
+++ Assignments/Assignment1/src/q.c
@@ -6,6 +6,24 @@
 
 #include "q.h"
 
+/* this struct is how the queue works */
+struct q_entry_t {
+    struct product *p;
+    struct q_entry_t *next;
+};
+
+static pthread_mutex_t q_lock = PTHREAD_MUTEX_INITIALIZER;
+
+static int maxSize = 0;
+static int count = 0;
+static struct q_entry_t *q;
+
+/* pre: takes in a positive integer 'size'
+ * post: intializes a queue of size 'size'
+ */
 void init_q(int size)
 {
+    maxSize = size;
+    count = 0;
+    q = NULL;
 }
