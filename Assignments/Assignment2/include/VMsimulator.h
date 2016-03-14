c980d14be95d3818cb20e5c0cd6f46037d4e9ecf
Bradford Smith <bsmith8@stevens.edu>
2016-03-14 12:03:58 -0400

    Assignment2: added Makefile and header templates

---
+++ Assignments/Assignment2/include/VMsimulator.h
@@ -0,0 +1,22 @@
+/* Bradford Smith (bsmith8)
+ * CS 492 Assignment 2 VMsimulator.h
+ * 03/14/2016
+ * "I pledge my honor that I have abided by the Stevens Honor System."
+ */
+
+#ifndef _VMSIMULATOR_H_
+#define _VMSIMULATOR_H_
+
+/* C: defaults
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+*/
+/* C++: defaults
+#include <string>
+#include <iostream>
+#include <algorithm>
+*/
+
+#endif /* _VMSIMULATOR_H_ */
