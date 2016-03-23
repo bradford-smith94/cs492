11af274520dbc96192ec1b5eb76907821632c390
Bradford Smith <bsmith8@stevens.edu>
2016-03-23 12:06:43 -0400

    Assignment2: built out the plot script more
    The plot script will now handle plotting multiple series to two graphs, all it
    needs is the data files to be setup. Which requires the program to properly
    output data.

--- Assignments/Assignment2/src/plot.sh
+++ Assignments/Assignment2/src/plot.sh
@@ -6,21 +6,61 @@
 # "I pledge my honor that I have abided by the Stevens Honor System."
 ################################################################################
 
-# This is a script to automate running tests and creating a graph based on the
+# This is a script to automate running trials and creating graphs based on the
 # results
 
+# Variables for filenames and trial results
 TEMP="./gnuplot.in"
 DATA="./data.dat"
-OUT="../graph.png"
-echo > $TEMP
+OUT1="../demand_graph.png"
+OUT2="../prepaging_graph.png"
+FIFO=""
+LRU=""
+CLOCK=""
 
+# Fill $DATA with FIFO,LRU and Clock results using demand paging for page sizes
+# 1, 2, 4, 8, and 16
+echo -n > $DATA
+
+# Create temporary gnuplot input file for demand paging plot
+echo -n > $TEMP
 echo "set xlabel \"Page Size\"" >> $TEMP
 echo "set ylabel \"Page Swaps\"" >> $TEMP
 echo "set term png" >> $TEMP
 echo "set key reverse Left outside" >> $TEMP
 echo "set grid" >> $TEMP
 echo "set style data linespoints" >> $TEMP
-echo "set output \"${OUT}\"" >> $TEMP
-echo "plot \"${DATA}\" using 1:2 title \"FIFO\"" >> $TEMP
+echo "set output \"${OUT1}\"" >> $TEMP
+echo "set title \"Demand Paging\"" >> $TEMP
+echo "plot \"${DATA}\" using 1:2 title \"FIFO\", \
+\"\" using 1:3 title \"LRU\", \
+\"\" using 1:4 title \"Clock\"" >> $TEMP
 
+# plot the demand paging graph
 gnuplot $TEMP
+
+# Fill $DATA with FIFO,LRU and Clock results using pre-paging for page sizes
+# 1, 2, 4, 8, and 16
+echo -n > $DATA
+
+# Create temporary gnuplot input file for pre-paging plot
+echo -n > $TEMP
+echo "set xlabel \"Page Size\"" >> $TEMP
+echo "set ylabel \"Page Swaps\"" >> $TEMP
+echo "set term png" >> $TEMP
+echo "set key reverse Left outside" >> $TEMP
+echo "set grid" >> $TEMP
+echo "set style data linespoints" >> $TEMP
+echo "set output \"${OUT2}\"" >> $TEMP
+echo "set title \"Pre-Paging\"" >> $TEMP
+echo "plot \"${DATA}\" using 1:2 title \"FIFO\", \
+\"\" using 1:3 title \"LRU\", \
+\"\" using 1:4 title \"Clock\"" >> $TEMP
+
+# plot the pre-paging graph
+gnuplot $TEMP
+
+# cleanup
+rm $TEMP
+rm $DATA
+
