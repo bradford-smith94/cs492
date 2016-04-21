4e1fc6ec71d2e8f47c5eeb8479f97fbf24ccafdd
Bradford Smith <bsmith8@stevens.edu>
2016-04-20 21:28:35 -0400

    Assignment3: rewrote parse_args with getopt
    Used getopt to parse options better than multiple loops over the argv list.

--- Assignments/Assignment3/src/parse_args.c
+++ Assignments/Assignment3/src/parse_args.c
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 492 Assignment 3 parse_args.c
- * 04/17/2016
+ * 04/20/2016
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -12,78 +12,52 @@
  *      -d <dir_list>
  *      -s <disk size>
  *      -b <block size>
- * post: parses args and sets up the global environment
+ * post: parses args using `getopt` and sets up the global environment
  */
 void parse_args(int argc, char** argv)
 {
-    int i;
+    int opt;
 
+    /* print usage if we don't have the correct number of arguments */
     if (argc != 9)
     {
         usage(argv[0]);
         exit(-1);
     }
 
-    /* search for '-f' */
-    for (i = 0; i < 9; i++)
-        if (!strcmp(argv[i], "-f"))
-            break;
-    if (i < 8)
+    /* parse options looking for 'f', 'd', 's' and 'b', each requiring an
+     *      argument which will be set in 'optarg'
+     */
+    while ((opt = getopt(argc, argv, "f:d:s:b:")) != -1)
     {
-        if ((gl.fd_flist = open(argv[i + 1], O_RDONLY)) < 0)
+        switch (opt)
         {
-            perror(argv[0]);
+            case 'f':
+                if ((gl.fd_flist = open(optarg, O_RDONLY)) < 0)
+                {
+                    perror(argv[0]);
 
-            exit(-1);
+                    exit(-1);
+                }
+                break;
+            case 'd':
+                if ((gl.fd_dlist = open(optarg, O_RDONLY)) < 0)
+                {
+                    perror(argv[0]);
+
+                    exit(-1);
+                }
+                break;
+            case 's':
+                gl.dsize = atoi(optarg);
+                break;
+            case 'b':
+                gl.bsize = atoi(optarg);
+                break;
+            default: /* invalid option */
+                usage(argv[0]);
+                exit(-1);
         }
-    }
-    else
-    {
-        usage(argv[0]);
-        exit(-1);
-    }
-
-    /* search for '-d' */
-    for (i = 0; i < 9; i++)
-        if (!strcmp(argv[i], "-d"))
-            break;
-    if (i < 8)
-    {
-        if ((gl.fd_dlist = open(argv[i + 1], O_RDONLY)) < 0)
-        {
-            perror(argv[0]);
-
-            exit(-1);
-        }
-    }
-    else
-    {
-        usage(argv[0]);
-        exit(-1);
-    }
-
-    /* search for '-s' */
-    for (i = 0; i < 9; i++)
-        if (!strcmp(argv[i], "-s"))
-            break;
-    if (i < 8)
-        gl.dsize = atoi(argv[i + 1]);
-    else
-    {
-        usage(argv[0]);
-        exit(-1);
-    }
-
-    /* search for '-b' */
-    for (i = 0; i < 9; i++)
-        if (!strcmp(argv[i], "-b"))
-            break;
-    if (i < 8)
-        gl.bsize = atoi(argv[i + 1]);
-    else
-    {
-        usage(argv[0]);
-        exit(-1);
     }
 }
 
