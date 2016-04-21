d8e399136d6bfd60d7ab029f9d236e5565c52f80
Bradford Smith <bsmith8@stevens.edu>
2016-04-20 22:02:16 -0400

    Assignment3: starting to setup main loop
    Main loop will read in commands of length 256 characters. This limit should be
    replaced with a defined macro.
    
    Still need to parse out the line and check commands.

--- Assignments/Assignment3/src/main.c
+++ Assignments/Assignment3/src/main.c
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 492 Assignment 3 main.c
- * 04/17/2016
+ * 04/20/2016
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -17,8 +17,52 @@
  */
 int main(int argc, char** argv)
 {
+    int n;
+    char* line;
+    char** v;
+
     /* parse args and setup global environment */
     parse_args(argc, argv);
+
+    /* main command reading loop */
+    line = (char*)malloc(256 * sizeof(char));
+    while (1)
+    {
+        /* prompt */
+        printf("> ");
+        fflush(stdout);
+
+        /* fd 0 is stdin */
+        if ((n = read(0, line, 256)) == -1)
+        {
+            perror(argv[0]);
+
+            fs_exit();
+            exit(-1);
+        }
+        else if (n == 0)
+        {
+            fs_exit();
+            break;
+        }
+        else
+        {
+            /* overwrite newline with NULL-byte */
+            line[n - 1] = '\0';
+
+#ifdef DEBUG
+            printf("Read line: <%s>\n", line);
+            fflush(stdout);
+#endif
+
+            /* TODO: use strtok to parse out the command? or turn 'line' into a
+             * vector
+             */
+        }
+
+        /* TODO: remove this break after checking for commands */
+        break;
+    }
 
     fs_exit();
     return 0;
