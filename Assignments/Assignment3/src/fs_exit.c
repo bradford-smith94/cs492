3862ef2519c73352bd4a4d3dc682690635dd5ea3
Bradford Smith <bsmith8@stevens.edu>
2016-05-02 23:06:26 -0400

    Assignment3: `exit` command now frees more memory
    Free the gl.ldisk and the gl.tree structures.

--- Assignments/Assignment3/src/fs_exit.c
+++ Assignments/Assignment3/src/fs_exit.c
@@ -1,6 +1,6 @@
 /* Bradford Smith (bsmith8)
  * CS 492 Assignment 3 fs_exit.c
- * 04/30/2016
+ * 05/02/2016
  * "I pledge my honor that I have abided by the Stevens Honor System."
  */
 
@@ -11,10 +11,50 @@
  */
 void fs_exit()
 {
+    node* n;
+    node* neighbors;
+    fs_file* f;
+    leaf* t;
+
     /* close open file descriptors */
     close(gl.fd_flist);
     close(gl.fd_dlist);
 
-    /* TODO: don't forget about this, leaking so much memory :( */
+    /* free gl.ldisk */
+    while ((n = popNode(&gl.ldisk)) != NULL)
+        free(n);
+
+    /* free gl.tree */
+    t = gl.tree;
+    neighbors = NULL;
+    while (t != NULL)
+    {
+        f = (fs_file*)(t->data);
+
+        /* free the file */
+        free(f->name);
+        while ((n = popNode(&(f->lfile))) != NULL)
+            free(n);
+        free(f);
+
+        if ((n = t->children) != NULL)
+        {
+            do
+            {
+                appendNode(&neighbors, createNode(n->data));
+            } while ((n = n->next) != NULL);
+        }
+
+        /* free the tree node */
+        free(t);
+
+        if ((n = popNode(&neighbors)) != NULL)
+        {
+            /* TODO: might be leaking memory from popping off of neighbors */
+            t = (leaf*)(n->data);
+        }
+        else
+            t = NULL;
+    }
 }
 
